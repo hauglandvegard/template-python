eed735135380f69ed550c918cf81f9409003bd89
Vegard <hauglandvegard@gmail.com>
2026-05-20 15:45:39 +0200

    Add 'B' to Ruff linting and ignore logs
    Add the 'B' (bugbear) linter to Ruff configuration and add the `logs/`
    directory to `.gitignore`.
    Also, remove unused `RESOURCES_DIR` and `DATA_DIR` variables from
    `config.py`.

--- src/config.py
+++ src/config.py
@@ -8,5 +8,3 @@ ROOT_DIR = Path(__file__).parent.parent.resolve()
 
 ENV_FILE = ROOT_DIR / ".env"
 LOGS_DIR = ROOT_DIR / "logs"
-RESOURCES_DIR = ROOT_DIR / "resources"
-DATA_DIR = RESOURCES_DIR / "data"
